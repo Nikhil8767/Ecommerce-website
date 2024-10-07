8cae003299853b73be281de8ddada675202df68b
Nikhil <nikhilgupta932003@gmail.com>
2024-10-07 23:31:23 +0530

    did some work in collection file

--- frontend/src/pages/Collections.jsx
+++ frontend/src/pages/Collections.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
 
 const Collections = () => {
+  const {products}=useContext(ShopContext);
   return (
-    <div>Collections</div>
+    <div>
+        <div className='flex-flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
+        
+        </div>
+    </div>
   )
 }
 
