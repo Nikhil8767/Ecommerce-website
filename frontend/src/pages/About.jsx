55ca82f9ecf57288ee2b241d121bce4c0616fb58
Nikhil <nikhilgupta932003@gmail.com>
2024-10-16 00:49:16 +0530

    about and contact us page is created

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsLetterbox from '../components/NewsLetterbox'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={'ABOUT'} text2={'US'}/>
+      </div>
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img src={assets.about_img} alt="" className='w-full md:max-w-[450px]' />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+        <p>this is the best shopping site made by Nikhil Gupta .for making or creating more sites like this contact him</p>
+        <p>you can also text him on his linkedin account .</p>
+        <b className='text-gray-800'>OUR MISSIONS</b>
+        <p>our main mission is to supply the best and the original quality of brand to the user where ever he needs it </p>
+        </div>
+      </div>
+
+      <div className='text-xl py-4' >
+        <Title text1={'WHY'} text2={'CHOOSE US'}/>
+      </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+      <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+        <b>Qualtity Assurence:</b>
+        <p className='text-gray-600'>the qualtiy which we provide is the best ever and you will find no such qualtity anywhere</p>
+      </div>
+      <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+        <b>Convinence:</b>
+        <p className='text-gray-600'>the qualtiy which we provide is the best ever and you will find no such qualtity anywhere</p>
+      </div>
+      <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+        <b>Exceptional customer:</b>
+        <p className='text-gray-600'>the qualtiy which we provide is the best ever and you will find no such qualtity anywhere</p>
+      </div>
+      </div>
+      <NewsLetterbox/>
+
+    </div>
   )
 }
 
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsLetterbox from '../components/NewsLetterbox'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+    <div className='text-center text-2xl pt-10 border-t'>
+      <Title text1={'CONTATCT'} text2={'US'}/>
+    </div>
+
+    <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
+      <img src={assets.contact_img} alt="" className='w-full md:max-w-[480px]' />
+      <div className='flex flex-col justify-center items-start gap-6'>
+        <p className='font-semibold text-xl text-gray-600'>Our Store</p>
+        <p className='text-gray-500'>santoshi mata mandir <br />nehru coloy pension nagar nagpur</p>
+        <p className='text-gray-500'>tel:(415)555-39393 <br />email:admin@gamil.com</p>
+        <p className='font-semibold text-xl text-gray-500'>Careers at forever</p>
+        <p className='text-gray-500'>learn more about us and jobs opening</p>
+       <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore job</button>
+      </div>
+    </div>
+    <NewsLetterbox/>
+    </div>
   )
 }
 
