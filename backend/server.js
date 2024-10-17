adec973b52e4890d90d82229dcae2261ddf0c785
Nikhil <nikhilgupta932003@gmail.com>
2024-10-17 22:07:47 +0530

    added the backend servers

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js';
+
+
+// app config 
+
+const app=express();
+const port=process.env.PORT || 4000
+
+// middlewares 
+app.use(express.json())
+app.use(cors())
+connectDB()
+
+
+// api endpoints 
+
+app.get('/',(req,res)=>{
+    res.send("api is working")
+})
+
+app.listen(port,()=>console.log("server startedd on port: "+port)
+)
