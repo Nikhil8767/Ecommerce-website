f205340cdd8fe11e834dbca5f074f10333126406
Nikhil <nikhilgupta932003@gmail.com>
2024-11-10 21:08:14 +0530

    added login for update cart in cartcontroller

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -39,9 +39,15 @@ const updateCart=async(req,res)=>{
     try {
         const {userId,itemId,size,quantity}=req.body
 
-        const us
+        const userData=await userModel.findById(userId)
+        let cartData=await userData.cartData;
+
+        cartData[itemId][size]=quantity
+        await userModel.findByIdAndUpdate(userId,{cartData})
+        res.json({success:true,message:"cart updated"})
     } catch (error) {
-        
+        console.log(error);
+        res.json({success:false,message:error.message})
     }
 
 }
